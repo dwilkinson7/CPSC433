e5abd80b6868cd9e0cee5aac59258e5c1c00b763
Andrejs Gusakovs <andrejs.gusakovs@gmail.com>
2016-12-07 12:47:03 -0700

    Commented and cleaned out the code

--- Person.java
+++ Person.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 import java.util.HashSet;
 
+/**
+ * Person class to represent the person. Within the class we store information about the role of the person
+ * which projects, groups the person is part of and is head of. If the person is smoker, hacker, researcher etc.
+ * Who the person works with and which room the person is assigned to. 
+ */
 
 public class Person {
 
@@ -17,6 +22,10 @@ public class Person {
 		private HashSet<Grp> groups;
 		private Room room;
 		
+		/**
+		 * Constructor method for Person
+		 * @param p - String represents name of the Person
+		 */
 		public Person(String p) {
 			this.name = p;
 			secretary = false;
@@ -32,96 +41,182 @@ public class Person {
 			
 		}
 
+		/**
+		 * Method to get the name of the person
+		 * @return name - String name of the Person
+		 */
 		public String getName(){
 			return this.name;
 		}
 
+		/**
+		 * Method to set the boolean that represents if the Person is a secretary
+		 */
 		public void setSecretary(){
 			this.secretary = true;
 		}
 
+		/**
+		 * Method to see if the Person is a secretary
+		 * @return secretary - boolean that shows if a person is a secretary
+		 */
 		public boolean getSecretary(){
 			return this.secretary;
 		}
 
+		/**
+		 * Method to set the boolean that represents if the Person is a researcher
+		 */
 		public void setResearcher(){
 			this.researcher = true;
 		}
 
+		/**
+		 * Method to see if the Person is a researcher
+		 * @return researcher - boolean that shows if a person is a researcher
+		 */
 		public boolean getResearcher(){
 			return this.researcher;
 		}
 
+		/**
+		 * Method to set the boolean that represents if the Person is a manager
+		 */
 		public void setManager(){
 			this.manager = true;
 		}
 
+		/**
+		 * Method to see if the Person is a manager
+		 * @return manager - boolean that shows if a person is a manager
+		 */
 		public boolean getManager(){
 			return this.manager;
 		}
 
+		/**
+		 * Method to set the boolean that represents if the Person is a smoker
+		 */
 		public void setSmoker(){
 			this.smoker = true;
 		}
 
+		/**
+		 * Method to see if the Person is a smoker
+		 * @return smoker - boolean that shows if a person is a smoker
+		 */
 		public boolean getSmoker(){
 			return this.smoker;
 		}
 
+		/**
+		 * Method to set the boolean that represents if the Person is a hacker
+		 */
 		public void setHacker(){
 			this.hacker = true;
 		}
 
+		/**
+		 * Method to see if the Person is a hacker
+		 * @return smoker - boolean that shows if a person is a hacker
+		 */
 		public boolean getHacker(){
 			return this.hacker;
 		}
 
+		/**
+		 * Method to add the group the Person is a part of
+		 * @param g - Group object
+		 */
 		public void addGroup(Grp g){
 			this.groups.add(g);
 		}
 		
-		
+		/**
+		 * Method to fetch the HashSet of the projects the Person is a part of
+		 * @return projects - HashSet of projects Person is a part of
+		 */
 		public HashSet<Project> getProjectsList(){
 			return this.projects;
 		}
 		
+		/**
+		 * Method to add the Project to the HashSet of project the person is a part of
+		 * @param proj - Project object that Person is a part of
+		 */
 		public void addProject(Project proj)	{
 			this.projects.add(proj);
 		}
 
+		/**
+		 * Method to add project to the HashSet of the projects Person is a head of
+		 * @param p - Project object that Person is a head of
+		 */
 		public void setProjectHead(Project p){
 			this.headproject.add(p);
 		}
 
+		/**
+		 * Method to get the HashSet of projects Person is a head of
+		 * @return headproject - HashSet of projects Person is a head of
+		 */
 		public HashSet<Project> getProjectHead(){
 			return this.headproject;
 		}
 
+		/**
+		 * Method to add group to the HashSet of the group Person is a head of
+		 * @param g - Group object the Person is a head of
+		 */
 		public void setGroupHead(Grp g){
 			this.headgroup.add(g);
 		}
 
+		/**
+		 * Method to get the HashSet of groups Person is a head of
+		 * @return headgroup - HashSet of groups Person is a head of
+		 */
 		public HashSet<Grp> getGroupHeadList(){
 			return this.headgroup;
 		}
 
+		/**
+		 * Metohd to get the HashSet of People the Person works with
+		 * @return worksWith - HashSet of Person that this Person works with
+		 */
 		public HashSet<Person> getWorksWith(){
 			return this.worksWith;
 		}
 		
+		/**
+		 * Method to add Person object to the HashSet that this Person works with
+		 * @param worksWith - Person object that this Person works with
+		 */
 		public void setWorksWith(Person worksWith) {
 			if (!this.worksWith.contains(worksWith))
 				this.worksWith.add(worksWith);
 		}
 
+		/**
+		 * Method to get the HashSet of groups the Person is in
+		 * @return group - HashSet of groups that Person is part of
+		 */
 		public HashSet<Grp> getGroupsList() {
 			return groups;
 		}
 
+		/**
+		 * Method to get the room Person is assigned to
+		 * @return room - Room object represents the Room that Person is assigned to
+		 */
 		public Room getRoom() {
 			return room;
 		}
 
+		/**
+		 * Method to set the Room that Person is assigned to
+		 * @param room - Room object that Person is assigned to
+		 */
 		public void setRoom(Room room) {
 			this.room = room;
 		}
--- Room.java
+++ Room.java
@@ -1,6 +1,12 @@
 import java.util.ArrayList;
 import java.util.HashSet;
 
+/**
+* Room class to represent the room. Within this class we store information about room size, 
+* people assigned to this room, close by rooms, 
+* the name of the room and the size of the room (default assumed to be medium).
+*/
+
 public class Room {
 
 	private String size;
@@ -9,6 +15,10 @@ public class Room {
 	private HashSet<Room> closeRooms = new HashSet<Room>();
 	private boolean full;
 	
+	/**
+	 * Room constructor method
+	 * @param name - room name
+	 */
 	public Room(String name)
 	{
 		this.name = name;
@@ -16,26 +26,49 @@ public class Room {
 		size = "medium";
 	}
 	
+	/**
+	 * Instead of hashing based on object id we hash based on String name
+	 * Makes hashing more predictable
+	 */
 	@Override
 	public int hashCode()
 	{
 		return this.getName().hashCode();
 	}
 	
+	/**
+	 * Method to check if the room is full
+	 * @return full - boolean to see if the room is full
+	 */
 	public boolean isFull()
 	{
 		return this.full;
 	}
+	
+	/**
+	 * Method to get room name 
+	 * @return name - String with the room name
+	 */
 	public String getName()
 	{
 		return this.name;
 	}
+	
+	/**
+	 * Method to assign person to the room
+	 * @param person - Person object, represents person we are assigning to this room
+	 */
 	public void assignPerson(Person person)
 	{
 		assigned.add(person);
 		if (person.getManager() == true || person.getProjectHead().size() > 0 || person.getGroupHeadList().size() > 0 || assigned.size() == 2)
 				this.full = true;
 	}
+	
+	/**
+	 * Method to remove person assigned to this room
+	 * @param person - Person object, represents person we remove from this room
+	 */
 	public void removePerson(Person person)
 	{
 		assigned.remove(person);
@@ -43,28 +76,49 @@ public class Room {
 			this.full = false;
 	}
 	
+	/**
+	 * Method to add room that is close to this Room to the HashSet of close rooms
+	 * @param room - Room object, represents room that is close to this room
+	 */
 	public void assignClose(Room room)
 	{
 		this.closeRooms.add(room);
 	}
 	
+	/**
+	 * Get the HashSet of close rooms
+	 * @return closeRooms - HashSet of close rooms
+	 */
 	public HashSet<Room> getCloseRooms()
 	{
 		return this.closeRooms;
 	}
 	
+	/**
+	 * Method to set the size of the room
+	 * @param str - string that represents the size of the room
+	 */
 	public void setRoomSize(String str)
 	{
 		this.size = str;
 	}
 	
+	/**
+	 * Method to get the room size
+	 * @return size - String that represents the size of the room
+	 */
 	public String getRoomSize()
 	{
 		return this.size;
 	}
 	
+	/**
+	 * Method to get the list of people assigned to this room
+	 * @return assigned - ArrayList of Person objects that represent people assigned to this room
+	 */
 	public ArrayList<Person> getAssigned()
 	{
 		return this.assigned;
 	}
 }
+
